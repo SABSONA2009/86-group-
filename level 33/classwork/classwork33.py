f992a49e7b1126bda014234b64a28cd94c95d605
SABSONA2009 <saba.salukvadze2000@gmail.com>
2026-02-08 21:26:42 +0400

    fourth commit

---
+++ level 33/classwork/classwork33.py
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+names=["saba", "davit", "nika", "ana", "gia"]
+
+
+user=input("enter your name: ")
+
+names.append(user)
+
+print(names)
+
+names.insert(3,"tarieli")
+
+print(names)
+
+
+names.pop(4)
+
+names.remove("saba")
+
+
+
+print(names)
+
+
+print(names.index(user))
+
+
+
+
---
+++ level 33/homework/homework33.py
@@ -0,0 +1,113 @@
+# # append() - სიის ბოლოში ამატებს ახალ ელემენტს
+# # insert() - სიის კონკრეტულ პოზიციაზე ამატებს ელემენტს
+# # pop() - შლის ელემენტს სიიდან (ნაგულისხმევად ბოლო ელემენტს)
+
+
+
+
+
+# numbers = [10, 20, 30, 40, 50]
+
+
+# print(len(numbers))
+
+
+
+
+
+
+
+# num1=int(input("enter your number: "))
+
+
+# num2=int(input("enter your number: "))
+
+
+# num3=int(input("enter your number: "))
+
+
+# num4=int(input("enter your number: "))
+
+
+# num5=int(input("enter your number: "))
+
+
+# number=(num1, num2, num3, num4, num5)
+
+
+
+# numbers.append(23)
+
+# print(numbers)
+
+# colors = ["red", "green", "blue", "yellow", "purple"]
+
+# colors.pop()  
+# print(colors)
+
+
+# animals = ["dog", "cat", "elephant", "lion"]
+
+# animals.insert(1, "monkey")
+# print(animals)
+
+
+# names=[]
+
+# name1=(input("enter your name: "))
+
+# name2=(input("enter your name: "))
+
+# name3=(input("enter your name: "))
+
+
+
+# names=[name1, name2, name3]
+
+# names.insert(0,"teacher")
+
+# names.pop(3)
+# print(names)
+
+
+
+
+
+def jami(a, b):
+    return a + b
+
+
+result = jami(5, 7)
+print(result)
+
+
+
+# def num (a):
+#     return num % 2 == 0
+
+
+# result=num(5)
+
+
+# if num % 2 == 0:
+#     print("luwia")
+
+# else:
+#     print("kenta")
+
+def kvadrati(a):
+    return a ** 2
+
+
+result=kvadrati(6)
+
+print(result)
+
+
+
+def didi_asoebi(text):
+    return text.upper()
+
+
+result = didi_asoebi("vai")
+print(result)
